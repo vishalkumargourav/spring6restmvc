5dfa792db0d26ea78ed70602c567c9a60677d7fe
vishalkumargourav <vishalkumargourav@github.com>
2025-01-14 15:39:39 +0530

    Initial Commit

---
+++ src/main/java/com/nathuncorp/spring6restmvc/Spring6restmvcApplication.java
@@ -0,0 +1,13 @@
+package com.nathuncorp.spring6restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6restmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6restmvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathuncorp/spring6restmvc/Spring6restmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathuncorp.spring6restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6restmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
