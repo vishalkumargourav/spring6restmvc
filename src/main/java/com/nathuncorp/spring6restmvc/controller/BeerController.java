e56c004107686cf74957c05b0a31268febcf48bb
vishalkumargourav <vishalkumargourav@github.com>
2025-01-15 03:09:14 +0530

    Added Headers While Creating New Beer

--- src/main/java/com/nathuncorp/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/nathuncorp/spring6restmvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.nathuncorp.spring6restmvc.model.Beer;
 import com.nathuncorp.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -25,7 +26,10 @@ public class BeerController {
 
         Beer savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
