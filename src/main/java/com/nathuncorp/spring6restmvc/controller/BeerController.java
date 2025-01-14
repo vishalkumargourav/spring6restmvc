53c4bf7551069f2e5677f96d240310293cdf4b9a
vishalkumargourav <vishalkumargourav@github.com>
2025-01-15 02:18:55 +0530

    Added RequestMapping at Class Level

--- src/main/java/com/nathuncorp/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/nathuncorp/spring6restmvc/controller/BeerController.java
@@ -5,7 +5,9 @@ import com.nathuncorp.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,19 +16,21 @@ import java.util.UUID;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         log.debug("List Beers - in controller");
 
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
         log.debug("Get Beer By ID - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
