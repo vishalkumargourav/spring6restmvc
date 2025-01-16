fc55e399af62d6e3a7068335cc7ce6958045cff5
vishalkumargourav <vishalkumargourav@github.com>
2025-01-17 02:49:29 +0530

    Added @ResponseStatus to Replace @ControllerAdvice

--- src/main/java/com/nathuncorp/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/com/nathuncorp/spring6restmvc/controller/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.nathuncorp.spring6restmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value is Not Found")
 public class NotFoundException extends RuntimeException{
     public NotFoundException() {
     }
