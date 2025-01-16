29023f1c6a3f615e336af80683201124bb14425c
vishalkumargourav <vishalkumargourav@github.com>
2025-01-16 07:57:29 +0530

    Completed Test Case with Mockito

--- src/test/java/com/nathuncorp/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/nathuncorp/spring6restmvc/controller/BeerControllerTest.java
@@ -1,20 +1,36 @@
 package com.nathuncorp.spring6restmvc.controller;
 
+import com.nathuncorp.spring6restmvc.service.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+//@SpringBootTest
+@WebMvcTest(BeerController.class)
+// NOTE : With above @WebMvcTest, it indicates that it is a TEST SPLICE.
+// We are also limiting that only BeerController should be brought up.
 class BeerControllerTest {
+    // @Autowired
+    // BeerController beerController;
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockitoBean
+    BeerService beerService;
 
     @Test
-    void getBeerById() {
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    void getBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        // System.out.println(beerController.getBeerById(UUID.randomUUID()));
     }
 }
