284b7c979be9f136b902b1260753dc5046a11b3b
vishalkumargourav <vishalkumargourav@github.com>
2025-01-16 07:45:34 +0530

    Changed to RequireArgsContructor

--- src/main/java/com/nathuncorp/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/nathuncorp/spring6restmvc/controller/BeerController.java
@@ -2,33 +2,32 @@ package com.nathuncorp.spring6restmvc.controller;
 
 import com.nathuncorp.spring6restmvc.model.Beer;
 import com.nathuncorp.spring6restmvc.service.BeerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
     @RequestMapping(value = "{beerId}", method = RequestMethod.DELETE)
-    public ResponseEntity deleteById(@PathVariable("beerId") UUID beerId){
+    public ResponseEntity deleteById(@PathVariable("beerId") UUID beerId) {
         beerService.deleteById(beerId);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(value = "{beerId}", method = RequestMethod.PUT)
-    public ResponseEntity updateById(@PathVariable("beerId") UUID beerId, @RequestBody Beer beer){
+    public ResponseEntity updateById(@PathVariable("beerId") UUID beerId, @RequestBody Beer beer) {
         beerService.updateBeerById(beerId, beer);
 
         HttpHeaders headers = new HttpHeaders();
@@ -38,7 +37,7 @@ public class BeerController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity saveNewBeer(@RequestBody Beer beer){
+    public ResponseEntity saveNewBeer(@RequestBody Beer beer) {
         log.debug("Save New Beer - in controller");
 
         Beer savedBeer = beerService.saveNewBeer(beer);
@@ -50,14 +49,14 @@ public class BeerController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Beer> listBeers(){
+    public List<Beer> listBeers() {
         log.debug("List Beers - in controller 1234");
 
         return beerService.listBeers();
     }
 
     @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
-    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
         log.debug("Get Beer By ID - in controller");
 
         return beerService.getBeerById(beerId);
--- src/main/java/com/nathuncorp/spring6restmvc/controller/CustomerController.java
+++ src/main/java/com/nathuncorp/spring6restmvc/controller/CustomerController.java
@@ -1,9 +1,8 @@
 package com.nathuncorp.spring6restmvc.controller;
 
-import com.nathuncorp.spring6restmvc.model.Beer;
 import com.nathuncorp.spring6restmvc.model.Customer;
 import com.nathuncorp.spring6restmvc.service.CustomerService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,26 +15,26 @@ import java.util.UUID;
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/customer")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
 
     @RequestMapping(value = "{customerId}", method = RequestMethod.DELETE)
-    public ResponseEntity deleteById(@PathVariable("customerId") UUID customerId){
+    public ResponseEntity deleteById(@PathVariable("customerId") UUID customerId) {
         customerService.deleteById(customerId);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(value = "{customerId}", method = RequestMethod.PUT)
-    public ResponseEntity updateCustomerById(@PathVariable("customerId") UUID customerId, @RequestBody Customer customer){
+    public ResponseEntity updateCustomerById(@PathVariable("customerId") UUID customerId, @RequestBody Customer customer) {
         customerService.updateCustomerById(customerId, customer);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity createNewCustomer(@RequestBody Customer newCustomer){
+    public ResponseEntity createNewCustomer(@RequestBody Customer newCustomer) {
         log.debug("Create New Customer - in controller");
 
         Customer savedCustomer = customerService.saveNewCustomer(newCustomer);
@@ -47,14 +46,14 @@ public class CustomerController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Customer> getAllCustomers(){
+    public List<Customer> getAllCustomers() {
         log.debug("List Customers - in controller");
 
         return customerService.getCustomers();
     }
 
     @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
-    public Customer getCustomerById(@PathVariable("customerId") UUID customerId){
+    public Customer getCustomerById(@PathVariable("customerId") UUID customerId) {
         log.debug("Get Customer by Id - in controller");
 
         return customerService.getCustomerById(customerId);
