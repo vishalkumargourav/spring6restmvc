bf41f9dd9e8838eb229be24961f74c1b90d3d4ec
vishalkumargourav <vishalkumargourav@github.com>
2025-01-22 23:44:44 +0530

    Added Entities

---
+++ src/main/java/com/nathuncorp/spring6restmvc/entities/Customer.java
@@ -0,0 +1,25 @@
+package com.nathuncorp.spring6restmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private UUID id;
+    @Version
+    private Integer version;
+    private String customerName;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
