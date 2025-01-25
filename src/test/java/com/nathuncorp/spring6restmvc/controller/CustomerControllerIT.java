b654374b8b7316bc3d573f950385001bdcf28722
vishalkumargourav <vishalkumargourav@github.com>
2025-01-25 15:07:18 +0530

    Refactored Code

--- src/test/java/com/nathuncorp/spring6restmvc/controller/CustomerControllerIT.java
+++ src/test/java/com/nathuncorp/spring6restmvc/controller/CustomerControllerIT.java
@@ -1,19 +1,16 @@
 package com.nathuncorp.spring6restmvc.controller;
 
-import com.nathuncorp.spring6restmvc.model.BeerDTO;
 import com.nathuncorp.spring6restmvc.model.CustomerDTO;
 import com.nathuncorp.spring6restmvc.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Repository;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CustomerControllerIT {
